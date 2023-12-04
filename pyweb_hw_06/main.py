d91ff74cfe5307576f7fa4518a1bcff1376aa852
Vitalii Savenko <vitaly.savenko12@gmail.com>
2023-12-04 16:47:38 +0200

    added merge func

--- pyweb_hw_06/main.py
+++ pyweb_hw_06/main.py
@@ -1,8 +1,17 @@
-
+from create_tables import create_tables
+from fill_data import insert_fake_data_to_database
+from querries import perform_querries
 
 
 def main():
+    # Create tables on DB
+    create_tables()
 
+    # Insert fake data to DB
+    insert_fake_data_to_database()
+
+    # Perform Hometask querries
+    perform_querries()
 
 
 if __name__ == "__main__":
